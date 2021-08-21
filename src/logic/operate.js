e733a88c8f97ca3deeb3413475ef9093c5eb3953
sheylaPozo <shiggipoke@gmail.com>
2021-08-21 09:34:02 -0500

    fix errors implementing TSE suggestion

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -13,10 +13,8 @@ export default function operate(numberOne, numberTwo, operation) {
     return one.times(two).toString();
   }
   if (operation === '÷') {
-    try {
-      return one.div(two).toString();
-    } catch {
-      return 'Error!';
+    if (numberTwo === '0') {
+      return 'Can\'t devide by 0';
     }
   }
   if (operation === '%') {
